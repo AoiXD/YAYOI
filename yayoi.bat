d84405693dff40881999d60234f60b15569b3a6b
AoiXD <84097627+AoiXD@users.noreply.github.com>
2023-06-25 21:18:55 +0900

    Update yayoi.bat

--- yayoi.bat
+++ yayoi.bat
@@ -1,1 +1,12 @@
-net start
+net stop MSSQL$YAYOI
+net stop SQLWrite
+net stop SSQLTELEMETRY$YAYOI
+net stop SQLBrowser
+
+@echo off
+timeout /nobreak 5 > /nul
+
+net start SQLWrite
+net start SSQLTELEMETRY$YAYOI
+net start SQLBrowser
+net start MSSQL$YAYOI
